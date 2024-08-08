f95ceae448af99191e6133235dc84032a1084c58
mohankrishna-challapalli <mohankrishna.challapalli@gmail.com>
2024-08-08 11:45:06 +0530

    added scanner files

--- src/FibonacciProgram.java
+++ src/FibonacciProgram.java
@@ -1,18 +1,41 @@
+import java.util.Scanner;
 
 public class FibonacciProgram {
-	public static void main(String args[])  
-	{    
-	 int n1=0,n2=1,n3,i,count=10;    
-	 System.out.print(n1+" "+n2);//printing 0 and 1    
-	    
-	 for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
-	 {    
-	  n3=n1+n2;    
-	  System.out.print(" "+n3);    
-	  n1=n2;    
-	  n2=n3;    
-	 }    
-	  
-	}
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Prompt user for the number of terms
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Fibonacci Series up to " + n + " terms (Iterative):");
+        printFibonacciIterative(n);
+
+        System.out.println("\nFibonacci Series up to " + n + " terms (Recursive):");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacciRecursive(i) + " ");
+        }
+
+        scanner.close();
+    }
+
+    // Iterative approach to print Fibonacci series
+    public static void printFibonacciIterative(int n) {
+        int firstTerm = 0, secondTerm = 1;
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + " ");
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+
+    // Recursive approach to calculate Fibonacci number
+    public static int fibonacciRecursive(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+    }
 }
--- src/ObjectDemo.java
+++ src/ObjectDemo.java
@@ -1,17 +1,53 @@
+//
+//public class ObjectDemo {
+//		
+//		static void method() {
+//			System.out.println("Hello ");
+//		}
+//		public void method2() {
+//			System.out.println("Hello ");
+//		}
+//		public static void main(String [] args) {
+//			method();
+//			ObjectDemo a=new ObjectDemo();
+//			a.method2();
+//		}
+//		
+//	}
 
-public class ObjectDemo {
-		
-		static void method() {
-			System.out.println("Hello ");
-		}
-		public void method2() {
-			System.out.println("Hello ");
-		}
-		public static void main(String [] args) {
-			method();
-			ObjectDemo a=new ObjectDemo();
-			a.method2();
-		}
-		
-	}
 
+class Car {
+    // Attributes of the Car class
+    String make;
+    String model;
+    int year;
+
+    // Constructor to initialize Car object
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Method to display Car details
+    public void displayDetails() {
+        System.out.println("Car Make: " + make);
+        System.out.println("Car Model: " + model);
+        System.out.println("Car Year: " + year);
+    }
+}
+
+public class ObjectDemo{
+    public static void main(String[] args) {
+        // Creating objects using the constructor
+        Car car1 = new Car("Toyota", "Corolla", 2020);
+        Car car2 = new Car("Honda", "Civic", 2022);
+
+        // Calling method to display details of each car
+        System.out.println("Details of Car 1:");
+        car1.displayDetails();
+
+        System.out.println("\nDetails of Car 2:");
+        car2.displayDetails();
+    }
+}
